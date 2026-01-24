d99cd0d6d2d67907f9bfb8fc519d1b0302a8145f
Pro-ma-r <info@promar.hr>
2026-01-24 11:01:38 +0100

    Update 006_views_web.sql

--- db/migrations/006_views_web.sql
+++ db/migrations/006_views_web.sql
@@ -1,1 +1,30 @@
-Database migrations for Tvornica vjenčanja
+-- Aktivni brendovi za web
+CREATE OR REPLACE VIEW active_brands_for_web AS
+SELECT DISTINCT
+  b.id   AS brand_id,
+  b.name AS brand_name,
+  b.activity,
+  b.email,
+  b.contact_person
+FROM brands b
+JOIN subscriptions_with_status s
+  ON s.brand_id = b.id
+WHERE s.status = 'AKTIVNA';
+
+
+-- Neaktivni (bivši) brendovi za web
+CREATE OR REPLACE VIEW former_brands_for_web AS
+SELECT
+  b.id   AS brand_id,
+  b.name AS brand_name,
+  b.activity,
+  b.email,
+  b.contact_person
+FROM brands b
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM subscriptions_with_status s
+  WHERE
+    s.brand_id = b.id
+    AND s.status = 'AKTIVNA'
+);
