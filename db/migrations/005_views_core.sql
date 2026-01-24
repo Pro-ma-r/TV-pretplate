65b42fb864c5e4c6164bf99cf87785192ec1f478
Pro-ma-r <info@promar.hr>
2026-01-24 11:01:05 +0100

    Update 005_views_core.sql

--- db/migrations/005_views_core.sql
+++ db/migrations/005_views_core.sql
@@ -1,1 +1,18 @@
-Database migrations for Tvornica vjenčanja
+-- View: subscriptions_with_status
+CREATE OR REPLACE VIEW subscriptions_with_status AS
+SELECT
+  s.id,
+  s.brand_id,
+  s.package_id,
+  s.start_date,
+  s.end_date,
+  s.payment_date,
+  s.manually_disabled,
+  s.disabled_note,
+  s.note,
+  CASE
+    WHEN s.manually_disabled = true THEN 'ISKLJUCENA'
+    WHEN CURRENT_DATE BETWEEN s.start_date AND s.end_date THEN 'AKTIVNA'
+    ELSE 'NEAKTIVNA'
+  END AS status
+FROM subscriptions s;
