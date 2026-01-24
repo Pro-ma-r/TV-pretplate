3d263617017d69bd794df0313ba57490d5ffea4d
Pro-ma-r <info@promar.hr>
2026-01-24 11:00:40 +0100

    Update 002_constraints.sql

--- db/migrations/002_constraints.sql
+++ db/migrations/002_constraints.sql
@@ -1,1 +1,12 @@
-Database migrations for Tvornica vjenčanja
+-- Omogućuje EXCLUSION constraint s datumskim rasponima
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+
+
+-- Zabrana vremenskog preklapanja istog paketa za isti brend
+ALTER TABLE subscriptions
+ADD CONSTRAINT no_overlapping_active_subscriptions
+EXCLUDE USING gist (
+  brand_id WITH =,
+  package_id WITH =,
+  daterange(start_date, end_date, '[]') WITH &&
+);
