1993abbb5a141aaa7e567551c5f09a920f0c5649
Pro-ma-r <info@promar.hr>
2026-01-24 11:00:15 +0100

    Update 003_triggers.sql

--- db/migrations/003_triggers.sql
+++ db/migrations/003_triggers.sql
@@ -1,1 +1,24 @@
-Database migrations for Tvornica vjenčanja
+-- Trigger funkcija: zabrana izmjene core polja pretplate
+CREATE OR REPLACE FUNCTION prevent_subscription_core_update()
+RETURNS trigger AS $$
+BEGIN
+  IF
+    NEW.brand_id   <> OLD.brand_id OR
+    NEW.package_id <> OLD.package_id OR
+    NEW.start_date <> OLD.start_date OR
+    NEW.end_date   <> OLD.end_date
+  THEN
+    RAISE EXCEPTION
+      'Nije dozvoljeno mijenjati brand, paket ili datume postojeće pretplate';
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Trigger
+CREATE TRIGGER trg_prevent_subscription_core_update
+BEFORE UPDATE ON subscriptions
+FOR EACH ROW
+EXECUTE FUNCTION prevent_subscription_core_update();
