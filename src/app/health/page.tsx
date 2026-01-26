b17557e07b74a4093e5c760c0fac7b010385d3c0
Pro-ma-r <info@promar.hr>
2026-01-26 21:43:47 +0100

    Delete src/app/health/page.tsx

--- src/app/health/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HealthPage() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>HEALTH CHECK OK</h1>
-      <p>Ako vidiš ovo, Next runtime je zdrav.</p>
-    </div>
-  );
-}
