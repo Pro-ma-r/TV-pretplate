1fa48b1fc70e3c24964b4457da7d0805d39dfc3d
Pro-ma-r <info@promar.hr>
2026-01-26 21:41:40 +0100

    Create page.tsx

---
+++ src/app/health/page.tsx
@@ -0,0 +1,8 @@
+export default function HealthPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>HEALTH CHECK OK</h1>
+      <p>Ako vidiš ovo, Next runtime je zdrav.</p>
+    </div>
+  );
+}
